4d8b8466542f7d075deb130dae46ca38d0fa57e6
RandyDpoe45 <36386903+RandyDpoe45@users.noreply.github.com>
2018-04-25 20:44:27 -0500

    Add files via upload

--- punto5/punto5.py
+++ punto5/punto5.py
@@ -12,9 +12,9 @@ def main():
     x=Symbol('x')
     f=diff(x*(np.e**x),x)
     e=f.subs({x:1})
-    for i in range(1,20):
+    for i in range(1,17):
         r=der2(1,1/(10**i))
-        print("|"+str(1/(10**i))+"|"+str(r))
+        print("|"+str(e)+"|"+str(1/(10**i))+"|"+str(r)+"|"+str(abs(e-r))+"|")
         
 if __name__ == "__main__":
     main()
