f953076aa303e855e3ea5265cf51aae90374a05d
RandyDpoe45 <36386903+RandyDpoe45@users.noreply.github.com>
2018-04-25 19:12:40 -0500

    Add files via upload

---
+++ punto5.py
@@ -0,0 +1,20 @@
+from sympy import *
+import numpy as np
+
+def der2(x1,h):
+    x=Symbol('x')
+    f=x*(np.e**x)
+    res=N(f.subs({x:x1+h}))
+    res2=N(f.subs({x:x1}))
+    return ((res-res2)/h)
+    
+def main():
+    x=Symbol('x')
+    f=diff(x*(np.e**x),x)
+    e=f.subs({x:1})
+    for i in range(1,20):
+        r=der2(1,1/(10**i))
+        print("|"+str(1/(10**i))+"|"+str(r))
+        
+if __name__ == "__main__":
+    main()
